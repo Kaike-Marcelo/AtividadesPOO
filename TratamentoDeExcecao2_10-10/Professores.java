3b403792bb36cc7192971cc8faee4b5fedc52a14
Kaike Marcelo <125518443+Kaike-Marcelo@users.noreply.github.com>
2023-10-10 20:10:12 -0300

    Terminar atividade Cleber

---
+++ TratamentoDeExcecao2_10-10/Professores.java
@@ -0,0 +1,81 @@
+import java.util.List;
+
+public class Professores {
+
+    private String nome;
+    private int idade;
+    private String sexo;
+    private String cpf;
+    private String rg;
+    private String endereco;
+    private List<Dependentes> dependentes;
+
+    public Professores (String nome, int idade, String sexo, String cpf, 
+                        String rg, String endereco) {
+
+        this.nome = nome;
+        this.idade = idade;
+        this.sexo = sexo;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.endereco = endereco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public List<Dependentes> getDependentes() {
+        return dependentes;
+    }
+
+    public void setDependentes(List<Dependentes> dependentes) {
+        this.dependentes = dependentes;
+    }
+
+    
+}
